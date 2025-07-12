03b25513ec44cc070ebb7ee2793b3d1a22c82305
veo8888 <veo8888@gmail.com>
2025-07-12 21:51:15 +0200

    Block-props is finished

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Product from './Product/Product';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+      <Product 
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product 
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+      <Product 
+        name="Yum-Yum"
+        imgUrl="https://cdn.pixabay.com/photo/2024/02/06/10/52/ai-generated-8556730_1280.jpg"
+        price={88.88}
+      />
+    </div>
+  );
+}
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,10 @@
+export default function Product({ name, imgUrl, price }) {
+    return (
+      <div>
+            <h2>{name}</h2>
+            <img src={imgUrl} alt={name} width="640" />
+        <p>Price: {price} credits</p>
+        <p>⭐⭐⭐⭐⭐</p>
+      </div>
+    );
+};
